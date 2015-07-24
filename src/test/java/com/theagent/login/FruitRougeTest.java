d70c6c83023cb95a066d95a748416efc8af9c2f8
The Agent <tech@theagent.com>
2015-07-24 14:24:44 +0200

    initialize the project

---
+++ src/test/java/com/theagent/login/FruitRougeTest.java
@@ -0,0 +1,83 @@
+package com.theagent.login;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.*;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.Assert.fail;
+
+public class FruitRougeTest {
+    private WebDriver driver;
+    private String baseUrl;
+    private boolean acceptNextAlert = true;
+    private StringBuffer verificationErrors = new StringBuffer();
+
+    @Before
+    public void setUp() throws Exception {
+        baseUrl = "http://mlamamra.theagent.fruitrouge.com";
+        driver = new RemoteWebDriver(
+                new URL("http://192.168.5.65:4444/wd/hub"),
+                DesiredCapabilities.firefox());
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    }
+
+    @Test
+    public void test() throws Exception {
+        driver.get(baseUrl + "/en/aauth/Aauth");
+        driver.findElement(By.id("email")).clear();
+        driver.findElement(By.id("email")).sendKeys("matthieu@theagent.com");
+        driver.findElement(By.id("password")).clear();
+        driver.findElement(By.id("password")).sendKeys("admin");
+        driver.findElement(By.xpath("//button[@type='submit']")).click();
+        Assert.assertEquals("matthieu@theagent.com", driver.findElement(By.cssSelector("span.userName")).getText());
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        driver.quit();
+        String verificationErrorString = verificationErrors.toString();
+        if (!"".equals(verificationErrorString)) {
+            fail(verificationErrorString);
+        }
+    }
+
+    private boolean isElementPresent(By by) {
+        try {
+            driver.findElement(by);
+            return true;
+        } catch (NoSuchElementException e) {
+            return false;
+        }
+    }
+
+    private boolean isAlertPresent() {
+        try {
+            driver.switchTo().alert();
+            return true;
+        } catch (NoAlertPresentException e) {
+            return false;
+        }
+    }
+
+    private String closeAlertAndGetItsText() {
+        try {
+            Alert alert = driver.switchTo().alert();
+            String alertText = alert.getText();
+            if (acceptNextAlert) {
+                alert.accept();
+            } else {
+                alert.dismiss();
+            }
+            return alertText;
+        } finally {
+            acceptNextAlert = true;
+        }
+    }
+}
