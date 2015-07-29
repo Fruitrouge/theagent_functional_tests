2dae55841e3d56d22e4cf80b83c4c7e633220528
The Agent <tech@theagent.com>
2015-07-29 17:04:45 +0200

    wait for page to be loaded

--- src/test/java/com/theagent/login/FruitRougeTest.java
+++ src/test/java/com/theagent/login/FruitRougeTest.java
@@ -1,5 +1,6 @@
 package com.theagent.login;
 
+import com.google.common.base.Function;
 import com.google.common.collect.ImmutableMap;
 import org.junit.After;
 import org.junit.Assert;
@@ -11,11 +12,14 @@ import org.openqa.selenium.chrome.ChromeDriverService;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
 
 import java.io.File;
 import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
 public class FruitRougeTest {
@@ -26,21 +30,31 @@ public class FruitRougeTest {
 
     @Before
     public void setUp() throws Exception {
-        baseUrl = "http://mlamamra.theagent.fruitrouge.com";
         driver = new ChromeDriver();
+        baseUrl = "http://mlamamra.theagent.fruitrouge.com/";
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     }
 
     @Test
-    public void test() throws Exception {
+    public void testFruitRougeSecond() throws Exception {
         driver.get(baseUrl + "/en/aauth/Aauth");
         driver.findElement(By.id("email")).clear();
         driver.findElement(By.id("email")).sendKeys("matthieu@theagent.com");
         driver.findElement(By.id("password")).clear();
         driver.findElement(By.id("password")).sendKeys("admin");
         driver.findElement(By.xpath("//button[@type='submit']")).click();
-        Assert.assertEquals("matthieu@theagent.com", driver.findElement(By.cssSelector("span.userName")).getText());
-        Thread.sleep(Integer.MAX_VALUE);
+        // Waiting 30 seconds for an element to be present on the page, checking
+        // for its presence once every 5 seconds.
+        Wait<WebDriver> wait = new FluentWait<WebDriver>(driver)
+                .withTimeout(30, TimeUnit.SECONDS)
+                .pollingEvery(5, TimeUnit.SECONDS)
+                .ignoring(NoSuchElementException.class);
+
+        WebElement foo = wait.until((driver) -> {
+                    return driver.findElement(By.cssSelector("body > nav > div.container > ul > li:nth-child(2) > a > span.userName"));
+                }
+        );
+        assertEquals("matthieu@theagent.com", driver.findElement(By.cssSelector("body > nav > div.container > ul > li:nth-child(2) > a > span.userName")).getText());
     }
 
     @After
